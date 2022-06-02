a64a15c578cde9f31432d07885d3d5d23489e223
JM <1910598john@gmail.com>
2022-06-02 13:35:03 +0800

    commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from tkinter import * #import tkinter GUI
 from time import strftime
 from tkinter import font
+from turtle import back
 
 #Toplevel windows boolean variables
 #purpose sani na boolean variables para pag gin click mo an 'book' or an 'customers' button san bisan pira ka beses, ka-usad lang sya ma create san window
@@ -34,7 +35,16 @@ def create_book_window():
         center_y = int(screen_height/2 - height/2)
         #tas i-seset naton sya
         book_window.geometry(f'{width}x{height}+{center_x}+{center_y}') #set book window's height and width
-        book_window.configure(bg='#D4CBC3') #main window background color
+        book_window.configure(bg='#FFFFFF') #main window background color
+
+        Label(book_window, text='Customer Data', font=('sans-serif', 25, font.BOLD), bg='#FFFFFF',fg='#787A40').place(x=170, y=30)
+        #first name
+        Label(book_window, text='First name:', background='#FFFFFF', font=('sans-serif', 10)).grid(column=0, row=0, sticky=E, pady=(120, 0), padx=(80, 0))
+        first_name_entry = Entry(book_window, width=15, highlightthickness=1, highlightbackground='#c0c4c1').grid(column=1, row=0, pady=(120, 0), padx=(0, 20))
+        #last name
+        Label(book_window, text='Last name:', background='#FFFFFF', font=('sans-serif', 10)).grid(column=2, row=0, sticky=E, pady=(120, 0))
+        last_name_entry = Entry(book_window, width=15, highlightthickness=1, highlightbackground='#c0c4c1').grid(column=3, row=0, pady=(120, 0))
+        
         book_window.mainloop() #mainloop() need ini sya para ma display an window
 
 #customers window
@@ -62,7 +72,7 @@ def create_customers_window():
         center_y = int(screen_height/2 - height/2)
         #tas i-seset naton sya
         customers_window.geometry(f'{width}x{height}+{center_x}+{center_y}') #customers window's height and width
-        customers_window.configure(bg='#D4CBC3') #main window background color
+        customers_window.configure(bg='#FFFFFF') #main window background color
         customers_window.mainloop() #mainloop() need ini sya para ma display an window
 
 #create main window
@@ -81,7 +91,7 @@ center_y = int(screen_height/2 - height/2)
 
 #tas i-seset naton sya
 main.geometry(f'{width}x{height}+{center_x}+{center_y}') #set main window's height and width
-main.configure(bg='#D4CBC3') #main window background color
+main.configure(bg='#FFFFFF') #main window background color
 
 #display time
 def start_timer(): #timer loop function
@@ -90,7 +100,7 @@ def start_timer(): #timer loop function
     time.after(1000, start_timer) #after method an first argument milliseconds ina 1000 ms = 1 second, tas an panduwa an start_timer function ica-call sya para mag loop after san 1000 milliseconds
 
 #time label widget
-time = Label(main, font=('sans-serif', 15, font.BOLD), fg='#787A40', bg='#D4CBC3') #(container, font, text color, background-color)
+time = Label(main, font=('sans-serif', 15, font.BOLD), fg='#787A40', bg='#FFFFFF') #(container, font, text color, background-color)
 time.grid(column=0, row=0, ipadx=20, ipady=20) #grid geometry manager, 3 ina sya (grid, pack, place) https://www.pythontutorial.net/tkinter/tkinter-pack/
 
 #book button
